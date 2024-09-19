94f279db4437138b34d1433ec2996dced1b91bc2
ouafcode <ouafaesaim@gmail.com>
2024-09-19 19:18:04 +0200

    add error handler

--- middlware/errorHandling.js
+++ middlware/errorHandling.js
@@ -1,28 +1,29 @@
-const errorHandling = (err, req, res, next) => {
-    err.statusCode = err.statusCode || 500;
-    err.status = err.status || 'error';
-    if(process.env.NODE_ENV == "development") {
-        sendError(err, res);
-    } else {
-        sendError_prod(err, res);
-    }
-};
 // for dev mode
-const sendError = (err, res)  => {
-    res.status(err.statusCode).json({
-        status: err.status,
-        error: err,
-        message: err.message,
-        stack: err.stack,
-     });
+const sendError = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
 };
 
 // for production
-const sendError_prod = (err, res)  => {
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message,
-     });
+const sendError_prod = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+};
+
+const errorHandling = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+  if (process.env.NODE_ENV === "development") {
+    sendError(err, res);
+  } else {
+    sendError_prod(err, res);
+  }
 };
 
 module.exports = errorHandling;
